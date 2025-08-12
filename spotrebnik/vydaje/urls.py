54613badea45627fdb708458c65b5c631d120b6b
p-mik <92926303+p-mik@users.noreply.github.com>
2025-08-12 23:21:32 +0200

    Add global navigation with login and logout

--- spotrebnik/vydaje/urls.py
+++ spotrebnik/vydaje/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import seznam_vydaju, pridat_vydaj, registrace, home, prihlaseni
+from .views import seznam_vydaju, pridat_vydaj, registrace, home, prihlaseni, odhlaseni
 
 urlpatterns = [
     path('', home, name='home'),  # Hlavní stránka
@@ -7,4 +7,5 @@ urlpatterns = [
     path('vydaje/pridat/', pridat_vydaj, name='pridat_vydaj'),
     path('registrace/', registrace, name='registrace'),  # Přidání URL pro registraci
     path('prihlaseni/', prihlaseni, name='prihlaseni'),
+    path('odhlasit/', odhlaseni, name='odhlaseni'),
 ]
