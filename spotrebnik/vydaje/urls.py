3953ee9ffac8783253c4d134ed530dd97f6b3873
p-mik <92926303+p-mik@users.noreply.github.com>
2025-08-13 14:10:55 +0200

    Add car management features

--- spotrebnik/vydaje/urls.py
+++ spotrebnik/vydaje/urls.py
@@ -1,10 +1,25 @@
 from django.urls import path
-from .views import seznam_vydaju, pridat_vydaj, registrace, home, prihlaseni, odhlaseni
+from .views import (
+    home,
+    odhlaseni,
+    pridat_auto,
+    pridat_vydaj,
+    prihlaseni,
+    registrace,
+    seznam_aut,
+    seznam_vydaju,
+    smazat_auto,
+    upravit_auto,
+)
 
 urlpatterns = [
     path('', home, name='home'),  # Hlavní stránka
     path('vydaje/', seznam_vydaju, name='seznam_vydaju'),
     path('vydaje/pridat/', pridat_vydaj, name='pridat_vydaj'),
+    path('auta/', seznam_aut, name='seznam_aut'),
+    path('auta/pridat/', pridat_auto, name='pridat_auto'),
+    path('auta/<int:id>/upravit/', upravit_auto, name='upravit_auto'),
+    path('auta/<int:id>/smazat/', smazat_auto, name='smazat_auto'),
     path('registrace/', registrace, name='registrace'),  # Přidání URL pro registraci
     path('prihlaseni/', prihlaseni, name='prihlaseni'),
     path('odhlasit/', odhlaseni, name='odhlaseni'),
